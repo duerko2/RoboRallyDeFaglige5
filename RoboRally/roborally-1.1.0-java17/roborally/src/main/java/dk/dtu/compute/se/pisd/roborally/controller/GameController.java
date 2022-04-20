1c5530b7150660965d25e283dc26c8a4e41501d7
Alexander Kiel Elsing <91070588+Daedalus1122@users.noreply.github.com>
2022-04-20 12:59:54 +0200

    checkForPush er færdig

--- RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
+++ RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
@@ -40,13 +40,14 @@ public class GameController {
     public GameController(@NotNull Board board) {
         this.board = board;
     }
+
     /**
      * This is just some dummy controller operation to make a simple move to see something
      * happening on the board. This method should eventually be deleted!
      *
      * @param space the space to which the current player should move
      */
-    public void moveCurrentPlayerToSpace(@NotNull Space space)  {
+    public void moveCurrentPlayerToSpace(@NotNull Space space) {
         // TODO Assignment V1: method should be implemented by the students:
         //   - the current player should be moved to the given space
         //     (if it is free()
@@ -55,10 +56,10 @@ public class GameController {
         //   - the counter of moves in the game should be increased by one
         //     if the player is moved
 
-        if(space.getPlayer()==null){
+        if (space.getPlayer() == null) {
             board.getCurrentPlayer().setSpace(space);
             board.nextPlayer();
-            board.setStep(board.getStep()+1);
+            board.setStep(board.getStep() + 1);
         }
 
     }
@@ -147,12 +148,12 @@ public class GameController {
     // XXX: V2
     private void executeNextStep() {
         Player currentPlayer = board.getCurrentPlayer();
-        if  (board.getPhase() == Phase.ACTIVATION && currentPlayer != null) {
+        if (board.getPhase() == Phase.ACTIVATION && currentPlayer != null) {
             int step = board.getStep();
             if (step >= 0 && step < Player.NO_REGISTERS) {
                 CommandCard card = currentPlayer.getProgramField(step).getCard();
                 if (card != null) {
-                    if (!card.command.getOptions().isEmpty()){
+                    if (!card.command.getOptions().isEmpty()) {
                         board.setPhase(Phase.PLAYER_INTERACTION);
 
                         return;
@@ -232,9 +233,9 @@ public class GameController {
      *
      * @param command This is the command chosen from the command option card.
      */
-    public void executeCommandOptionAndContinue(Command command){
+    public void executeCommandOptionAndContinue(Command command) {
         board.setPhase(Phase.ACTIVATION);
-        executeCommand(board.getCurrentPlayer(),command);
+        executeCommand(board.getCurrentPlayer(), command);
         int step = board.getStep();
         int nextPlayerNumber = board.getPlayerNumber(board.getCurrentPlayer()) + 1;
         if (nextPlayerNumber < board.getPlayersNumber()) {
@@ -256,82 +257,168 @@ public class GameController {
     public void moveForward(@NotNull Player player) {
         Space nextSpace;
         Space currentScape = player.getSpace();
-        switch (player.getHeading()){
-            case SOUTH -> player.setSpace(board.getSpace(player.getSpace().x,player.getSpace().y+1));
-            case WEST -> player.setSpace(board.getSpace(player.getSpace().x-1,player.getSpace().y));
-            case NORTH -> player.setSpace(board.getSpace(player.getSpace().x,player.getSpace().y-1));
-            case EAST -> player.setSpace(board.getSpace(player.getSpace().x+1,player.getSpace().y));
+        switch (player.getHeading()) {
+            case SOUTH:
+                nextSpace = (board.getSpace(player.getSpace().x, player.getSpace().y + 1));
+                checkForPush(player, nextSpace, player.getHeading());
+                break;
+            case WEST:
+                nextSpace = (board.getSpace(player.getSpace().x - 1, player.getSpace().y));
+                checkForPush(player, nextSpace, player.getHeading());
+                break;
+            case NORTH:
+                nextSpace = (board.getSpace(player.getSpace().x, player.getSpace().y - 1));
+                checkForPush(player, nextSpace, player.getHeading());
+                break;
+            case EAST:
+                nextSpace = (board.getSpace(player.getSpace().x + 1, player.getSpace().y));
+                checkForPush(player, nextSpace, player.getHeading());
+                break;
         }
     }
 
     // TODO Assignment V2
     public void fastForward(@NotNull Player player) {
-        for (int i = 0; i <= 2; i++) {
+        Space nextSpace;
+        Space currentScape = player.getSpace();
+        for (int i = 0; i < 2; i++) {
             switch (player.getHeading()) {
-                case SOUTH -> player.setSpace(board.getSpace(player.getSpace().x, player.getSpace().y + 1));
-                case WEST -> player.setSpace(board.getSpace(player.getSpace().x - 1, player.getSpace().y));
-                case NORTH -> player.setSpace(board.getSpace(player.getSpace().x, player.getSpace().y - 1));
-                case EAST -> player.setSpace(board.getSpace(player.getSpace().x + 1, player.getSpace().y));
+                case SOUTH:
+                    nextSpace = (board.getSpace(player.getSpace().x, player.getSpace().y + 1));
+                    checkForPush(player, nextSpace, player.getHeading());
+                    break;
+                case WEST:
+                    nextSpace = (board.getSpace(player.getSpace().x - 1, player.getSpace().y));
+                    checkForPush(player, nextSpace, player.getHeading());
+                    break;
+                case NORTH:
+                    nextSpace = (board.getSpace(player.getSpace().x, player.getSpace().y - 1));
+                    checkForPush(player, nextSpace, player.getHeading());
+                    break;
+                case EAST:
+                    nextSpace = (board.getSpace(player.getSpace().x + 1, player.getSpace().y));
+                    checkForPush(player, nextSpace, player.getHeading());
+                    break;
             }
         }
 
     }
 
-    public void moveThree(@NotNull Player player){
-        for (int i = 0; i <= 3; i++) {
-            switch (player.getHeading()) {
-                case SOUTH -> player.setSpace(board.getSpace(player.getSpace().x, player.getSpace().y + 1));
-                case WEST -> player.setSpace(board.getSpace(player.getSpace().x - 1, player.getSpace().y));
-                case NORTH -> player.setSpace(board.getSpace(player.getSpace().x, player.getSpace().y - 1));
-                case EAST -> player.setSpace(board.getSpace(player.getSpace().x + 1, player.getSpace().y));
+    public void moveThree(@NotNull Player player) {
+        Space nextSpace;
+        Space currentScape = player.getSpace();
+
+        for (int i = 0; i < 3; i++) {
+                switch (player.getHeading()) {
+                    case SOUTH:
+                        nextSpace = (board.getSpace(player.getSpace().x, player.getSpace().y + 1));
+                        checkForPush(player, nextSpace, player.getHeading());
+                        break;
+                    case WEST:
+                        nextSpace = (board.getSpace(player.getSpace().x - 1, player.getSpace().y));
+                        checkForPush(player, nextSpace, player.getHeading());
+                        break;
+                    case NORTH:
+                        nextSpace = (board.getSpace(player.getSpace().x, player.getSpace().y - 1));
+                        checkForPush(player, nextSpace, player.getHeading());
+                        break;
+                    case EAST:
+                        nextSpace = (board.getSpace(player.getSpace().x + 1, player.getSpace().y));
+                        checkForPush(player, nextSpace, player.getHeading());
+                        break;
+                }
             }
         }
-    }
 
-    public void backUp(@NotNull Player player){
-        switch (player.getHeading()){
-            case SOUTH -> player.setSpace(board.getSpace(player.getSpace().x,player.getSpace().y-1));
-            case WEST -> player.setSpace(board.getSpace(player.getSpace().x+1,player.getSpace().y));
-            case NORTH -> player.setSpace(board.getSpace(player.getSpace().x,player.getSpace().y+1));
-            case EAST -> player.setSpace(board.getSpace(player.getSpace().x-1,player.getSpace().y));
+        public void backUp (@NotNull Player player){
+        Space nextSpace;
+        Boolean backwards = true;
+            switch (player.getHeading()) {
+                case SOUTH:
+                    nextSpace = (board.getSpace(player.getSpace().x, player.getSpace().y -1));
+                    checkForPush(player, nextSpace,Heading.NORTH, backwards);
+                    break;
+                case WEST:
+                    nextSpace = (board.getSpace(player.getSpace().x + 1, player.getSpace().y));
+                    checkForPush(player, nextSpace, Heading.EAST, backwards);
+                    break;
+                case NORTH:
+                    nextSpace = (board.getSpace(player.getSpace().x, player.getSpace().y + 1));
+                    checkForPush(player, nextSpace, Heading.SOUTH, backwards);
+                    break;
+                case EAST:
+                    nextSpace = (board.getSpace(player.getSpace().x - 1, player.getSpace().y));
+                    checkForPush(player, nextSpace, Heading.WEST, backwards);
+                    break;
+            }
         }
-    }
 
-    public void uTurn(@NotNull Player player){
-        player.setHeading((player.getHeading().next()));
-        player.setHeading((player.getHeading().next()));
-    }
-
-    // TODO Assignment V2
-    public void turnRight(@NotNull Player player) {
-        player.setHeading(player.getHeading().next());
-    }
-
-    // TODO Assignment V2
-    public void turnLeft(@NotNull Player player) {
-        player.setHeading(player.getHeading().prev());
-    }
-
-    public boolean moveCards(@NotNull CommandCardField source, @NotNull CommandCardField target) {
-        CommandCard sourceCard = source.getCard();
-        CommandCard targetCard = target.getCard();
-        if (sourceCard != null && targetCard == null) {
-            target.setCard(sourceCard);
-            source.setCard(null);
-            return true;
-        } else {
-            return false;
+        public void uTurn (@NotNull Player player){
+            player.setHeading((player.getHeading().next()));
+            player.setHeading((player.getHeading().next()));
         }
+
+        // TODO Assignment V2
+        public void turnRight (@NotNull Player player){
+            player.setHeading(player.getHeading().next());
+        }
+
+        // TODO Assignment V2
+        public void turnLeft (@NotNull Player player){
+            player.setHeading(player.getHeading().prev());
+        }
+
+        public boolean moveCards (@NotNull CommandCardField source, @NotNull CommandCardField target){
+            CommandCard sourceCard = source.getCard();
+            CommandCard targetCard = target.getCard();
+            if (sourceCard != null && targetCard == null) {
+                target.setCard(sourceCard);
+                source.setCard(null);
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+    public void checkForPush(@NotNull Player player, @NotNull Space space, @NotNull Heading heading) {
+        Heading orig = heading;
+        Player other = space.getPlayer();
+        if (other != null) {
+            Space target = board.getNeighbour(other.getSpace(),orig);
+            if (target != null) {
+                    checkForPush(other, target, player.getHeading());
+
+                }
+            }
+
+        player.setSpace(space);
+    }
+    public void checkForPush(@NotNull Player player, @NotNull Space space, @NotNull Heading heading, boolean backwards) {
+        Player other = space.getPlayer();
+        if (other != null) {
+            Space target = board.getNeighbour(other.getSpace(),player.getHeading().next().next());
+            if (target != null) {
+
+                        checkForPush(other, target,player.getHeading().next().next(),backwards);
+
+
+
+
+
+            }
+        }
+        player.setSpace(space);
     }
 
 
-    /**
-     * A method called when no corresponding controller operation is implemented yet. This
-     * should eventually be removed.
-     */
-    public void notImplemented() {
-        // XXX just for now to indicate that the actual method is not yet implemented
-        assert false;
+        /**
+         * A method called when no corresponding controller operation is implemented yet. This
+         * should eventually be removed.
+         */
+        public void notImplemented () {
+            // XXX just for now to indicate that the actual method is not yet implemented
+            assert false;
+        }
+
     }
 
-}
