022feb7b84ded18835037701f55f0630aac895eb
Alex Batten <alex.batten@live.com>
2022-06-03 16:27:33 +0200

    Cleaned up the project. getting ready to add springboot

--- Lecture03/w3_task2/src/bmi/DataException.java
+++
@@ -1,9 +0,0 @@
-package bmi;
-
-public class DataException extends Exception {
-    private static final long serialVersionUID = 3184201770220225838L;
-
-    public DataException() {
-        super();
-    }
-}
