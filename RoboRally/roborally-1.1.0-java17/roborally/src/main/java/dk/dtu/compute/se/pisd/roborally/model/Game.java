211ba88c38e9a1ac784d6e8ef3aa08f3c1a51dfa
Nikolaj <Nikolajbeier@gmail.com>
2022-06-06 13:12:05 +0200

    Added game class

--- RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Game.java
+++ RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Game.java
@@ -6,4 +6,38 @@ public class Game {
     boolean readyToReceivePlayers;
     Board board;
 
+    public Game(Board board, int serialNumber, int maxAmountOfPlayers, boolean readyToReceivePlayers){
+        this.board = board;
+        this.serialNumber = serialNumber;
+        this.maxAmountOfPlayers = maxAmountOfPlayers;
+        this.readyToReceivePlayers = readyToReceivePlayers;
+    }
+
+    //Setters
+    public void setBoard(Board board){
+        this.board = board;
+    }
+    public void setSerialNumber(int serialNumber){
+        this.serialNumber = serialNumber;
+    }
+    public void setReadyToReceivePlayers(boolean bool){
+        this.readyToReceivePlayers = bool;
+    }
+    public void setMaxAmountOfPlayers(int maxAmountOfPlayers){
+        this.maxAmountOfPlayers = maxAmountOfPlayers;
+    }
+
+    //Getters
+    public Board getBoard(){
+        return this.board;
+    }
+    public int getSerialNumber(){
+        return this.serialNumber;
+    }
+    public int getMaxAmountOfPlayers(){
+        return this.maxAmountOfPlayers;
+    }
+    public boolean getReadyToReceivePlayers(){
+        return this.readyToReceivePlayers;
+    }
 }
