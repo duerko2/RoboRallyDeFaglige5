15c65a3bb0b98b3340ea1b51d45abfc069ad6f49
Holopopi <s215830@student.dtu.dk>
2022-06-08 10:25:25 +0200

    9 cards now

--- RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Player.java
+++ RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Player.java
@@ -35,7 +35,7 @@ import static dk.dtu.compute.se.pisd.roborally.model.Heading.SOUTH;
 public class Player extends Subject {
 
     final public static int NO_REGISTERS = 5;
-    final public static int NO_CARDS = 8;
+    final public static int NO_CARDS = 9;
 
     final public Board board;
 
