fb255b317a4f86ef9d252941c03096b67d62d619
Andreas Aagaard <s215830@student.dtu.dk>
2022-06-10 22:40:02 +0200

    ,1 added

--- RoboRally/roborally-1.1.0-java17/roborally/src/test/java/dk/dtu/compute/se/pisd/roborally/controller/GameControllerTest.java
+++ RoboRally/roborally-1.1.0-java17/roborally/src/test/java/dk/dtu/compute/se/pisd/roborally/controller/GameControllerTest.java
@@ -18,7 +18,7 @@ class GameControllerTest {
     private Game game;
     @BeforeEach
     void setUp() {
-        Board board = new Board(TEST_WIDTH, TEST_HEIGHT);
+        Board board = new Board(TEST_WIDTH, TEST_HEIGHT,1);
         gameController = new GameController(0, game);
         for (int i = 0; i < 6; i++) {
             Player player = new Player(board, null,"Player " + i,0);
